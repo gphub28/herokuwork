cbc2f5dce750e62088ad1c679bc9719f6efbdfed
gphub28 <142899962+gphub28@users.noreply.github.com>
2023-08-29 17:46:25 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Join Pakistan Army with Pakistan Forces</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            line-height: 1.6;
+            margin: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            min-height: 100vh;
+            background-color: #f2f2f2;
+        }
+
+        .container {
+            max-width: 1200px;
+            text-align: center;
+            padding: 20px;
+        }
+
+        h1 {
+            color: #0066cc;
+            margin-bottom: 20px;
+        }
+
+        p {
+            font-size: 18px;
+            color: #333;
+            text-align: center;
+            margin: 10px 0;
+            line-height: 1.8;
+        }
+
+        a {
+            color: #0066cc;
+            text-decoration: none;
+        }
+
+        a:hover {
+            text-decoration: underline;
+        }
+
+        button {
+            background-color: #0066cc;
+            color: #fff;
+            border: none;
+            padding: 10px 20px;
+            font-size: 16px;
+            cursor: pointer;
+        }
+
+        button:hover {
+            background-color: #0052a3;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <h1>Join Pakistan Army with Pakistan Forces</h1>
+        <p>Pakistan Forces provides comprehensive information on joining the Pakistan Army. From eligibility criteria to application procedures, we guide you through the process. Embark on a journey of service, honor, and dedication. Join the ranks of Pakistan's defenders.</p>
+        <p>For more details, visit the <a href="https://pakistanforces.com/join-pak-army/">Join Pak Army</a>.</p>
+
+        <div>
+        </div>
+    
+    </div>
+</body>
+
+</html>
