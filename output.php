3da68e9886441fd22ca39b60a1a08df3b70f5105
gphub28 <142899962+gphub28@users.noreply.github.com>
2023-08-29 15:26:18 +0500

    Output.php

--- output.php
+++ output.php
@@ -1,7 +1,8 @@
-<?php 
-echo '<h1><strong>An analysis of the Pakistani Army</strong></h1>
-<p>Pakistan\'s Army plays a crucial role in the nation\'s defense and security. There is a glorious history to the organization, and it is dedicated to protecting the country\'s sovereignty. There is a growing respect for the army both at home and abroad. The purpose of this blog is to explore the importance and role of the Pakistan Army, as well as the opportunities one can pursue within the organization.</p>
-<p><br></p>
-<p>Together with the creation of the nation, the Pakistan Army was created in 1947. A significant part of Pakistan\'s armed forces, it is considered to be one of the most respected and essential. This organization is primarily responsible for defending the country\'s borders, maintaining internal security, and supporting the nation\'s development efforts during peacetime. It is the army\'s responsibility to safeguard Pakistan\'s territorial integrity and to safeguard the national interests of Pakistan in times of war.</p>
-<p><br></p>
-<p>It is a dream for many young individuals to <a href="https://pakistanforces.com/join-pak-army/">Join Pak Army</a> and serve their country with honor and valor. There are several entry paths available in the army, each designed to suit the needs of individuals with varying educational backgrounds and levels of experience.</p>';
+<?php
+echo "<h1><strong>An analysis of the Pakistani Army</strong></h1>\n";
+echo "<p>Pakistan\'s Army plays a crucial role in the nation\'s defense and security. There is a glorious history to the organization, and it is dedicated to protecting the country\'s sovereignty. There is a growing respect for the army both at home and abroad. The purpose of this blog is to explore the importance and role of the Pakistan Army, as well as the opportunities one can pursue within the organization.</p>\n";
+echo "<p><br></p>\n";
+echo "<p>Together with the creation of the nation, the Pakistan Army was created in 1947. A significant part of Pakistan\'s armed forces, it is considered to be one of the most respected and essential. This organization is primarily responsible for defending the country\'s borders, maintaining internal security, and supporting the nation\'s development efforts during peacetime. It is the army\'s responsibility to safeguard Pakistan\'s territorial integrity and to safeguard the national interests of Pakistan in times of war.</p>\n";
+echo "<p><br></p>\n";
+echo "<p>It is a dream for many young individuals to <a href=\"https://pakistanforces.com/join-pak-army/\">Join Pak Army</a> and serve their country with honor and valor. There are several entry paths available in the army, each designed to suit the needs of individuals with varying educational backgrounds and levels of experience.</p>\n";
+?>
