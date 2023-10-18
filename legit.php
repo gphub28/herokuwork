61d0bff9dba12818157c44b28f252971b5b5b64c
gphub28 <142899962+gphub28@users.noreply.github.com>
2023-10-19 00:09:21 +0500

    Add files via upload

---
+++ legit.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Signal App</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            line-height: 1.6;
+            margin: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            min-height: 100vh;
+            background-color: #f2f2f2;
+        }
+
+        .container {
+            max-width: 1200px;
+            text-align: center;
+            padding: 20px;
+        }
+
+        h1 {
+            color: #0066cc;
+            margin-bottom: 20px;
+        }
+
+        p {
+            font-size: 18px;
+            color: #333;
+            text-align: center;
+            margin: 10px 0;
+            line-height: 1.8;
+        }
+
+        a {
+            color: #0066cc;
+            text-decoration: none;
+        }
+
+        a:hover {
+            text-decoration: underline;
+        }
+
+        button {
+            background-color: #0066cc;
+            color: #fff;
+            border: none;
+            padding: 10px 20px;
+            font-size: 16px;
+            cursor: pointer;
+        }
+
+        button:hover {
+            background-color: #0052a3;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <h1>Signal Social Application</h1>
+        <p>Signal is a privacy-focused messaging app that has gained prominence due to its strong encryption and commitment to user privacy. Developed by the Signal Foundation and Signal Messenger LLC, it is available on various platforms like Android, iOS, and desktop.</p>
+        <p>For more details, learn how to know if : <a href="https://legitopinions.com/someone-blocked-you-on-signal/">Someone blocked you on Signal</a>.</p>
+
+        <div>
+
+    </div>
+</body>
+
+</html>
